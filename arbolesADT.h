d047bb0b4fad6a093bfb9b9aed95836c74997dc5
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-07-10 09:56:57 -0300

    se agregaron comentarios

--- arbolesADT.h
+++ arbolesADT.h
@@ -7,13 +7,25 @@
 
 typedef struct arbolesCDT * arbolesADT;
 
+//Crea una nueva lista
 arbolesADT newArbolList(void);
+
+//Añade un árbol a la lista. SI ya está, actualiza los parámetros
 void addArbol(arbolesADT arboles, const char * comuna, const char * nombre, long int diametro);
+
+//Setea el current en first
 void toBeginArbol (arbolesADT arboles);
+
+//0 si current es nulo, 1 si no
 int hasNextArbol(arbolesADT arboles);
-char * nextNombreArbol (arbolesADT arboles);
+
+//Devuelve el nombre del árbol
+char * nombreArbol (arbolesADT arboles);
+
+//Devuelve el diámetro promedio y pasa al siguiente
 double nextDiametro (arbolesADT arboles);
+
+//Libera la lista
 void freeArboles(arbolesADT arboles);
-size_t sizeArbol (arbolesADT arboles);
 
 #endif /* arbolesADT_h */
