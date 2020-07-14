302a3dfa6a2e766e5a18768b82c5f100607ec354
ManuelDizen <67920263+ManuelDizen@users.noreply.github.com>
2020-07-14 15:50:00 -0300

    Update barriosADT.c

--- barriosADT.c
+++ barriosADT.c
@@ -16,10 +16,11 @@ typedef struct barriosCDT{
     size_t sizeBarrios;
 } barriosCDT;
 
+/*Crea un nuevo conjunto de barrios*/
 barriosADT nuevoBarrio(void){
     return calloc(1, sizeof(barriosCDT));
 }
-
+/*Busca el barrio. Devuelve 1 si lo encontro, 0 en caso contrario*/
 static int findBarrio (TBarrios * barrios, size_t size, char * nombre, int * index){
     for (int i = 0; i < size; i++){
         if (strcmp(barrios[i].nombre, nombre) == 0){
@@ -30,6 +31,8 @@ static int findBarrio (TBarrios * barrios, size_t size, char * nombre, int * ind
     return 0;
 }
 
+/*Busca el barrio, si lo encuentra, incrementa la cantidad de arboles, actualiza el diametro promedio y retorna 0.
+Si no, retorna 1*/
 int incArbolBarrio(barriosADT barrios, char * nombre){
     int index;
     if (findBarrio(barrios->barrios, barrios->sizeBarrios, nombre, &index)){
@@ -42,6 +45,7 @@ int incArbolBarrio(barriosADT barrios, char * nombre){
         
 }
 
+/*Agrega un barrio si no existia antes*/
 int addBarrio (barriosADT barrios, char * nombre, long int cant_hab){
     int index;
     errno = 0;
@@ -63,6 +67,7 @@ int addBarrio (barriosADT barrios, char * nombre, long int cant_hab){
     return -1;
 }
 
+/*Libera la memoria utilizada*/
 void freeBarrios(barriosADT barrios){
     for (int i = 0; i < sizeBarrio(barrios); i++)
         free(barrios->barrios[i].nombre);
@@ -70,18 +75,21 @@ void freeBarrios(barriosADT barrios){
     free(barrios);
 }
 
+/*Devuelve la cantidad de barrios*/
 size_t sizeBarrio (barriosADT barrios){
     return barrios->sizeBarrios;
 }
 
+/*Devuelve el nombre del barrio en la posicion index*/
 char * nombreBarrio (barriosADT barrios, size_t index){
     return barrios->barrios[index].nombre;
 }
 
+/*Devuelve la cantidad de arboles que hay en el barrio que esta en index*/
 long int cantArb (barriosADT barrios, size_t index){
     return barrios->barrios[index].cant_arboles;
 }
-
+/*Devuelve el promedio de arboles por habitante del barrio que esta en index*/
 double promedioArbHab (barriosADT barrios, size_t index){
     return barrios->barrios[index].arbol_habitante_promedio;
 }
