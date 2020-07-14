a6a3277b1cc03e39192f19062b0253bce962e173
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-07-14 17:45:49 -0300

    se agregaron comentarios

--- barriosADT.h
+++ barriosADT.h
@@ -6,28 +6,28 @@
 
 typedef struct barriosCDT * barriosADT;
 
-/*Crea un nuevo conjunto de barrios*/
+//Crea un nuevo conjunto de barrios
 barriosADT nuevoBarrio(void);
 
-/*Agrega un barrio*/
+//Agrega un barrio al vector. Si ya estaba, retorna -1. Si no hay memoria suficente, retorna 1. Si no hay problemas, retorna 0
 int addBarrio (barriosADT barrios, char * nombre, long int cant_hab);
 
-/*Agrega un arbol al barrio correspondiente*/
+//Agrega un arbol al barrio correspondiente. Si no encuentra el barrio retorna 1, caso contrario retorna 0.
 int incArbolBarrio(barriosADT barrios, char * nombre);
 
-/*Devuelve la cantidad de barrios*/
+//Devuelve la cantidad de barrios
 size_t sizeBarrio (barriosADT barrios);
 
-/*Devuelve el nombre del barrio*/
+//Devuelve el nombre del barrio en la posición index
 char * nombreBarrio (barriosADT barrios, size_t index);
 
-/*Devuelve la cantidad de arboles de un barrio*/
+//Devuelve la cantidad de arboles del barrio en la posición index
 long int cantArb (barriosADT barrios, size_t index);
 
-/*Devuelve la cantidad de arboles que hay por habitante de cierto barrio*/
+//Devuelve la cantidad de arboles que hay por habitante en el barrio en la posición index
 double promedioArbHab (barriosADT barrios, size_t index);
 
-/*Libera la memoria reservada por el conjunto de barrios*/
+//Libera la memoria reservada por el conjunto de barrios
 void freeBarrios(barriosADT barrios);
 
 
