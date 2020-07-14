ba88f84ee7a46233d15fecf53ddee1c05c0309c3
ManuelDizen <67920263+ManuelDizen@users.noreply.github.com>
2020-07-14 15:02:27 -0300

    Update arbolesADT.h

--- arbolesADT.h
+++ arbolesADT.h
@@ -8,11 +8,22 @@
 
 typedef struct arbolesCDT * arbolesADT;
 
+/*Crea un nuevo conjunto de arboles*/
 arbolesADT nuevoArbol(void);
+
+/*Agrega un arbol al conjunto*/
 void addArbol (arbolesADT arboles, char * nombre, long int diametro);
+
+/*Devuelve la cantidad de arboles*/
 size_t sizeArboles (arbolesADT arboles);
+
+/*Devuelve el nombre del arbol en la posicion index*/
 char * nombreArbol (arbolesADT arboles, size_t index);
+
+/*Devuelve el promedio diametro del arbol en la posicion index*/
 double promedioDiam (arbolesADT arboles, size_t index);
+
+/*Libera la memoria reservada por el conjunto de arboles*/
 void freeArboles(arbolesADT arboles);
 
 
