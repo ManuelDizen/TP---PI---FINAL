1cd194ea363db9ccfff184bc9d0d9ef3983fee25
ManuelDizen <67920263+ManuelDizen@users.noreply.github.com>
2020-07-14 14:56:13 -0300

    Update barriosADT.h

--- barriosADT.h
+++ barriosADT.h
@@ -6,13 +6,28 @@
 
 typedef struct barriosCDT * barriosADT;
 
+/*Crea un nuevo conjunto de barrios*/
 barriosADT nuevoBarrio(void);
+
+/*Agrega un barrio*/
 void addBarrio (barriosADT barrios, char * nombre, long int cant_hab);
+
+/*Agrega un arbol al barrio correspondiente*/
 void incArbolBarrio(barriosADT barrios, char * nombre);
+
+/*Devuelve la cantidad de barrios*/
 size_t sizeBarrio (barriosADT barrios);
+
+/*Devuelve el nombre del barrio*/
 char * nombreBarrio (barriosADT barrios, size_t index);
+
+/*Devuelve la cantidad de arboles de un barrio*/
 long int cantArb (barriosADT barrios, size_t index);
+
+/*Devuelve la cantidad de arboles que hay por habitante de cierto barrio*/
 double promedioArbHab (barriosADT barrios, size_t index);
+
+/*Libera la memoria reservada por el conjunto de barrios*/
 void freeBarrios(barriosADT barrios);
 
 
